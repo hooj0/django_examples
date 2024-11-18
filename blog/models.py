366977d2dbaac2f213d902bff793b31f4115b92e
hoojo <hoojo@qq.com>
2024-11-18 17:34:04 +0800

    feat(blog): 创建 Post 模型
    - 添加 Post 类，包含标题、内容、创建日期、发布日期、图片、作者和状态字段- 实现 publish 方法，用于更新发布日期
    - 添加 __str__ 方法，返回文章标题

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    content = models.TextField(blank=False, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='media/images/')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    status = models.BooleanField(default=False, db_comment='Published or not')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    __str__ = lambda self: self.title
