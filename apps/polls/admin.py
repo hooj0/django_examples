2ce7bef3a1d2c73bc8814b147a3e997927338e97
hoojo <hoojo@qq.com>
2024-11-26 13:42:42 +0800

    feat(admin): 优化 Question 模型的 admin 页面并添加 Choice模型
    - 在 admin 中注册 Choice 模型
    - 创建 ChoiceInline 类，以表格形式显示 Choice 对象
    - 创建 QuestionAdmin 类，自定义 Question 模型的 admin 页面布局
    - 添加 list_display、list_filter 和 search_fields 属性，增强 admin 页面功能

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from apps.polls.models import Question
+from apps.polls.models import Question, Choice
+
 
 # Register your models here.
-admin.site.register(Question)
+# StackedInline 允许在同一行中添加或删除字段
+# TabularInline 允许在表格中添加或删除字段
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    classes = ['collapse']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Info', {'fields': ['published_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'published_date', 'was_published_recently')
+    list_filter = ['published_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
