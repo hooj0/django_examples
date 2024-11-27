217a56ffcb033609d6ac004f1e33871e42b9ffa1
hoojo <hoojo@qq.com>
2024-11-27 15:49:51 +0800

    feat(admin): 在 admin.py 中注册新模型
    - 注册了 Choices、Comment、Book 和 Author 模型
    - 使这些模型能够在 Django 后台中进行管理和显示

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Choices, Comment, Book, Author
 
 admin.site.register(Post)
+admin.site.register(Choices)
+admin.site.register(Comment)
+admin.site.register(Book)
+admin.site.register(Author)
