0314cdb6ca664e3c627ce598e75324a30f1b0627
hoojo <hoojo@qq.com>
2024-11-27 09:29:05 +0800

    test(blog): 更新 Category 模型单元测试
    - 继承 BasedTestCase 以复用通用测试设置
    - 移除 setUpTestData 方法，改为在 setUp 中调用父类 setUp
    - 删除了未使用的测试方法 test_category_name_max_length 和 test_category_str_representation
    - 保留并添加了对 Category 类型和货币的测试

--- apps/blog/tests/test_category_model.py
+++ apps/blog/tests/test_category_model.py
@@ -1,23 +1,13 @@
 from django.test import TestCase
 
 from apps.blog.models import Category
+from apps.blog.tests.tests import BasedTestCase, output
 
 
-class CategoryModelTest(TestCase):
+class CategoryModelTest(BasedTestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods.
-        Category.objects.create(category_name=None)
-
-    def test_category_name_max_length(self):
-        category = Category.objects.get(id=1)
-        max_length = category._meta.get_field('name').max_length
-        self.assertEqual(max_length, 100)  # Assuming the name field has a max_length of 100
-
-    def test_category_str_representation(self):
-        category = Category.objects.get(id=1)
-        self.assertEqual(str(category), 'TestCategory')  # Check if the string representation is correct
+    def setUp(self):
+        super().setUp()
 
     def test_category_choices(self):
         choices = Category.CategoryType
@@ -28,3 +18,7 @@ class CategoryModelTest(TestCase):
         print(choices.names)
         print(choices.values)
         print(choices.labels)
+
+    def test_category_model(self):
+        category = Category.objects.create(category_name=Category.CategoryType.SW, category_text=Category.get_currencies()[0][0], category_type=Category.CategoryType.KP)
+        output(category)
