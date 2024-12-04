3f397bc1a38cc1171194781b70402c6404b28b59
hoojo <hoojo@qq.com>
2024-12-04 11:36:48 +0800

    test(blog): 测试一对多数据关系模型
    - 新增测试用例以验证一多对数据关系的行为
    - 添加了对不同 on_delete 选项的测试，包括 CASCADE, PROTECT, RESTRICT, SET_NULL, DO_NOTHING 和 SET
    - 实现了正向和反向查询的测试
    - 测试了多对多关系的 CRUD 操作

--- apps/blog/tests/test_association_book_o2m.py
+++ apps/blog/tests/test_association_book_o2m.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import reset_queries
 
 from apps.blog.models import Author, Book, Post, faker, Tags, Studio
-from apps.blog.tests.tests import BasedTestCase, sql_decorator
+from apps.blog.tests.tests import BasedTestCase, sql_decorator, output_sql
 
 
 class BookModelTest(BasedTestCase):
@@ -83,3 +83,99 @@ class BookModelTest(BasedTestCase):
         # book_set 名称 和 book的外键 related_name="books" 关联
         # 触发SQL
         print(author.books.all())
+
+    def test_delete_cascade(self):
+        author = Author.mock_data()
+        author.save()
+        print(Author.objects.all())
+
+        book = Book.mock_data()
+        book.save()
+
+        author.books.add(book)
+        print(Book.objects.all())
+
+        # on_delete=models.CASCADE：删除 author，级联删除 book
+        author.delete()
+        print(Book.objects.all())   # empty
+        print(Author.objects.all()) # empty
+
+        if True:
+            return
+
+        # on_delete=models.PROTECT：删除 author，ProtectedError 防止删除被引用对象
+        author.delete() # django.db.models.deletion.ProtectedError: ("Cannot delete some instances of model 'Author' because they are referenced through protected foreign keys: 'Book.author'.", {<Book: _state: unknown, id: 1>})
+
+        # on_delete=models.RESTRICT：删除 author，RestrictedError 来防止删除被引用的对象
+        author.delete() # django.db.models.deletion.RestrictedError: ("Cannot delete some instances of model 'Author' because they are referenced through restricted foreign keys: 'Book.author'.", {<Book: _state: unknown, id: 1>})
+
+        # on_delete=models.SET_NULL：删除 author，级联的数据关系被设置Null
+        author.delete()
+        print(Book.objects.all())   # <QuerySet [<Book: _state: unknown, id: 1, title: Mock Book *****, price: -589.29, author_id: None>]>
+        print(Author.objects.all()) # <QuerySet []>
+
+        # on_delete=models.DO_NOTHING：删除 author，IntegrityError 未做任何操作，引用完整性不在
+        author.delete()
+        print(Book.objects.all())   # <QuerySet [<Book: _state: unknown, id: 1, title: Mock Book *****, price: 364.15, author_id: 1>]>
+        print(Author.objects.all()) # empty
+
+        # on_delete=models.SET(get_user_id)：删除 author，修改重置关联的数据
+        author.delete()
+
+    def test_m2o_query(self):
+        author, book = self.prepare_data()
+        reset_queries()
+
+        print("-------------------正向查询--------------------")
+        print(author)
+        print(book)
+
+        output_sql(book.author)
+
+        author2 = Author.mock_data()
+        author2.save()
+        book.author = author2
+        print(book)
+        output_sql(book.author)
+
+        output_sql(Book.objects.select_related().all())
+        output_sql(Book.objects.select_related().get(pk=1))
+        output_sql(Book.objects.select_related("author"))
+
+    def test_m2o_query_reverse(self):
+        author, book = self.prepare_data()
+        reset_queries()
+
+        print("-------------------反向查询--------------------")
+        print(Author.objects.filter(book__title__contains='Mock Book'))
+        print(author.books.all())
+
+    def test_m2o_crud(self):
+        # 创建
+        author, book = self.prepare_data()
+
+        print(book)
+        print(author)
+        print(author.books.all())
+
+        # 取消关联
+        book.author = None # author_id: None
+        book.save()
+        # author.books.remove(book)
+        print(book)
+        print(author.books.all())   # <QuerySet []>
+
+        # 清空关联
+        author.books.set([])
+        author.books.clear()
+        print(author.books.all())
+
+    def prepare_data(self):
+        author = Author.mock_data()
+        author.save()
+
+        book = Book.mock_data()
+        book.save()
+
+        author.books.add(book)
+        return author, book
